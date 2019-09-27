56985959c4dabd4f0a757a709012553e80054d67
Naveen Raina <naveenraina@outlook.com>
2019-09-27 23:34:46 +0530

    comming app.vue with one more toggle control

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
+
   <div id="app">
-    <toggle-switch value='On'/>
+      {{switch1Value}}
+    <toggle-switch group="a" :options="myOptions" @change="log($event.value)" v-model="switch1Value"/>
+    <toggle-switch group="b" :options="myOptions1" @change="log($event.value)" v-model="switch2Value"/>
+    {{switch2Value}}
   </div>
 </template>
 
@@ -11,6 +15,41 @@ export default {
   name: 'app',
   components: {
     ToggleSwitch
+  },
+  data() {
+      return {
+          switch1Value: 'unknown',
+          switch2Value: 'unknown',
+        myOptions: {
+            items: {
+                delay: .4,
+                preSelected: 'unknown',
+                disabled: false,
+                labels: [
+                { name: 'Off', color: 'white', backgroundColor: 'red' },
+                { name: 'unknown', color: 'white', backgroundColor: 'grey' },
+                { name: 'On', color: 'white', backgroundColor: 'green' }
+                ]
+            }
+        },
+        myOptions1: {
+            items: {
+                delay: .4,
+                preSelected: 'unknown',
+                disabled: false,
+                labels: [
+                { name: 'Off', color: 'white', backgroundColor: 'red' },
+                { name: 'unknown', color: 'white', backgroundColor: 'grey' },
+                { name: 'On', color: 'white', backgroundColor: 'green' }
+                ]
+            }
+        }
+    }
+  },
+  methods:{
+      log(m){
+          console.log(m)
+      }
   }
 }
 </script>
